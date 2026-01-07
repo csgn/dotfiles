4b857521adb525318424ae910433dca1bf1d61e2
csgn <dev.csgn@gmail.com>
2026-01-07 18:14:48 +0300

    update config

--- nvim/lua/csgn/plugins/rainbowdelim.lua
+++ nvim/lua/csgn/plugins/rainbowdelim.lua
@@ -1,30 +1,31 @@
 return {
-    "HiPhish/rainbow-delimiters.nvim",
-    lazy = true,
-    event = {"BufReadPre", "BufNewFile"},
-    config = function()
-        require("rainbow-delimiters.setup").setup({
-            strategy = {
-                [''] = 'rainbow-delimiters.strategy.global',
-                vim = 'rainbow-delimiters.strategy.local',
-            },
-            query = {
-                [''] = 'rainbow-delimiters',
-                lua = 'rainbow-blocks',
-            },
-            priority = {
-                [''] = 110,
-                lua = 210,
-            },
-            highlight = {
-                'RainbowDelimiterRed',
-                'RainbowDelimiterYellow',
-                'RainbowDelimiterBlue',
-                'RainbowDelimiterOrange',
-                'RainbowDelimiterGreen',
-                'RainbowDelimiterViolet',
-                'RainbowDelimiterCyan',
-            },
-        })
-    end,
+	"HiPhish/rainbow-delimiters.nvim",
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("rainbow-delimiters.setup").setup({
+			strategy = {
+				[""] = "rainbow-delimiters.strategy.global",
+				vim = "rainbow-delimiters.strategy.local",
+			},
+			query = {
+				[""] = "rainbow-delimiters",
+				lua = "rainbow-blocks",
+			},
+			priority = {
+				[""] = 110,
+				lua = 210,
+			},
+			highlight = {
+				"RainbowDelimiterRed",
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterViolet",
+				"RainbowDelimiterCyan",
+			},
+		})
+	end,
 }
+
