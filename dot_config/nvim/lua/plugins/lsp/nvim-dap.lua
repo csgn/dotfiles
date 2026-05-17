48ed04bac69b459815006e4a05c37b3b379d97ba
csgn <dev.csgn@gmail.com>
2026-05-17 21:35:22 +0300

    Update conf

--- dot_config/nvim/lua/plugins/lsp/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/lsp/nvim-dap.lua
@@ -25,5 +25,53 @@ return {
 			function() require("dap").terminate() end,
 			desc = "Terminate"
 		},
-	}
+	},
+	config = function()
+		local dap = require("dap")
+		dap.adapters.coreclr = {
+			type = 'executable',
+			command = 'netcoredbg',
+			args = { '--interpreter=vscode' }
+		}
+
+		dap.configurations.cs = {
+			{
+				type = "coreclr",
+				name = "Launch for C# Project",
+				request = "launch",
+				program = function()
+					-- Automatically find dlls in any bin/ directory
+					local root = vim.fn.getcwd()
+					local dlls = vim.fn.globpath(root, "**/bin/**/*.dll", 0, 1)
+
+					if #dlls == 0 then
+						return vim.fn.input('Path to dll: ', root .. '/', 'file')
+					elseif #dlls == 1 then
+						return dlls[1]
+					else
+						-- If multiple exist (e.g., net6.0 and net8.0), let you pick
+						return vim.fn.input('Select DLL: ', dlls[1], 'file')
+					end
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtEntry = false,
+			},
+			{
+				type = "coreclr",
+				name = "Launch for Godot Game",
+				request = "launch",
+				program = "/usr/local/bin/godot",
+				args = {
+					"--path", "${workspaceFolder}",
+				},
+				cwd = "${workspaceFolder}",
+				env = {
+					-- This tells the .NET runtime inside Godot to wait for a debugger
+					-- to connect on this specific port.
+					DOTNET_DebuggerAgent = "transport=dt_socket,address=127.0.0.1:4711,server=y,suspend=n"
+				},
+				stopAtEntry = false,
+			}
+		}
+	end
 }
